27f58aa91418c2072edfc5f74882cf04d705865b
kes demata <kkfdemata03@gmail.com>
2023-09-28 21:54:18 +0800

    Added save cart in localStorage

--- part2-amazon/data/cart.js
+++ part2-amazon/data/cart.js
@@ -1,10 +1,21 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-},{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')); 
+
+if(!cart){
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  },{
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+
+
+// localstorage
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 //  Function Add to Cart
 export function addToCart(productId){
@@ -29,6 +40,7 @@ export function addToCart(productId){
       quantity: 1
     });
   }
+  saveToStorage();
 }
 
 
@@ -45,4 +57,6 @@ export function removeProductFromCart(productId){
   });
   //  replace the cart above
   cart = newCart;
+
+  saveToStorage();
 }
